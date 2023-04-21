63944003568cffa681f755ef2302fb1d5542effd
Olusegun007 <131487247+Olusegun007@users.noreply.github.com>
2023-04-21 20:30:52 +0100

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,19 @@
+--INSERT INTO EmployeeDemographics VALUES
+--(1002, 'Wale', 'Tunde', 25, 'Male'),
+--(1003, 'Ola', 'Tunmise', 29, 'Female'),
+--(1004, 'Michelle', 'Obama', 24, 'Female'),
+--(1005, 'Emeka-Okafor', 'Nancy', 32, 'Female'),
+--(1006, 'Paul', 'Nicholas', 36, 'Male'),
+--(1007, 'Adewumi', 'Adebisi', 28, 'Female'),
+--(1008, 'Olatian', 'Adewale', 27, 'Male'),
+--(1009, 'Emeka', 'Emmanuel', 21, 'Male')
+
+INSERT INTO EmployeeSalary VALUES
+(1002, 'Janitor', 20000),
+(1003, 'Frontdesk Off', 35000),
+(1004, 'Officer Asst', 20000),
+(1005, 'Branch Mgr', 70000),
+(1006, 'Marketing Mgr', 65000),
+(1007, 'HR', 60000),
+(1008, 'Area Sales Mgr', 45000),
+(1009, 'Branding Mgr', 50000)
